ee9b26e9f797863aeefd66a92879a0beff7d3df2
supriya909 <supiya_nallasura@yahoo.com>
2019-09-14 19:40:10 -0500

    Checkpassfail

---
+++ CheckPassFailProject/src/com/h2kinfosys/passfail/CheckPassFail.java
@@ -0,0 +1,14 @@
+package com.h2kinfosys.passfail;
+
+public class CheckPassFail {
+public static void main(String []args) {
+	int mark=100;
+	if (mark>=50){ 
+		System.out.println("pass");
+	}
+	else {
+		System.out.println("FAIL");
+	}
+	System.out.println("DONE");
+	}
+}
